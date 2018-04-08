35a6ad6f8c99987aecb985eec4cbdf809aac9a48
adebeo <info@adebeo.com>
2018-04-08 18:36:14 +0200

    context menu

--- adebeo_extensionName/view/adebeo_toolbar.rb
+++ adebeo_extensionName/view/adebeo_toolbar.rb
@@ -20,32 +20,27 @@ If you set a isToolbar to true You must puts icons files:
 =end
 
 module Adebeo::ExtensionName
-  
     def self.toolbar(isDevelloppement) 
 
         #get the extension name
-        extensionName = self.to_s.split("::").last
+        @extensionName = self.to_s.split("::").last
 
         #get command definition from the json inside cmdInToolBar.json
         cmdOfExtension = Adebeo::getHashFromJsonFile "#{File.dirname(__FILE__)}/toolbarAndCmd.json"
 
         #create menu if needed
         arrayOdIsMenu = cmdOfExtension.select{|k,v| v[:isMenu] ==true}
-        @plugins_menu = UI.menu("Plugins").add_submenu(extensionName) if (arrayOdIsMenu.length > 0 or isDevelloppement)
+        @plugins_menu = UI.menu("Plugins").add_submenu(@extensionName) if (arrayOdIsMenu.length > 0 or isDevelloppement)
         
         #create toolbar if needed
         arrayOdIsToolbar = cmdOfExtension.select{|k,v| v[:isToolbar] ==true}
-        @toolbar = UI::Toolbar.new extensionName if arrayOdIsToolbar.length >0     
+        @toolbar = UI::Toolbar.new @extensionName if arrayOdIsToolbar.length >0     
         
         #create context if needed
         arrayOdIsContext = cmdOfExtension.select{|k,v| v[:isContext] ==true}
-        if arrayOdIsToolbar.length >0
-            UI.add_context_menu_handler do |context_menu|
-                @subContextMenu = context_menu.add_submenu(extensionName)
-            end
-        end
 
         #create all commande
+        cmd_contexts = {}
         cmdOfExtension.each do |k,v|
             spec = {
                 :name => k.to_s,
@@ -54,8 +49,22 @@ module Adebeo::ExtensionName
             }
             spec[:submenu] = @plugins_menu if v[:isMenu]
             spec[:toolbar] = @toolbar if v[:isToolbar]
-            spec[:subContextMenu] = @subContextMenu if v[:isContext]
-            createCommand(spec)
+            spec[:subContextMenu] = v[:isContext] if v[:isContext]
+
+            cmdAdebeo = createCommand(spec)
+            if v[:isContext]
+                cmd_contexts[v[:description]] = "#{v[:command]}"
+            end
+        end
+
+
+        if arrayOdIsContext.length > 0
+            UI.add_context_menu_handler do |context_menu|
+                subcontextmenu = context_menu.add_submenu(@extensionName)
+                cmd_contexts.each{|description,cmdAdebeo|
+                    subcontextmenu.add_item(description) {eval(cmdAdebeo)}
+                }
+            end
         end
 
         # create option command in menu 
@@ -71,9 +80,10 @@ module Adebeo::ExtensionName
 
         # if develloppement mode create a reloader for controler
         if isDevelloppement
-            extenionName = self.to_s.downcase.gsub("::","_")
+            @extenionName = self.to_s.downcase.gsub("::","_")
             p = "#{File.dirname(__FILE__)}/../controlers/**/*.rb"
-            commandLine = "Dir.glob('#{p}').each{|f| load f};SKETCHUP_CONSOLE.clear()"
+            lib = "#{File.dirname(__FILE__)}/../adebeo_library.rb"
+            commandLine = "Dir.glob('#{p}').each{|f| load f};load '#{lib}';SKETCHUP_CONSOLE.clear()"
             spec = {
                     :name => "reload",
                     :description=>"Reload All File",
@@ -86,27 +96,28 @@ module Adebeo::ExtensionName
     end
   
     def self.createCommand(spec)
-        puts "create command"
-        puts spec.inspect
+        #puts "create command"
+        #puts spec.inspect
+        #iS_OSX = Sketchup.platform == :platform_osx
+        #iS_OSX ? cursorExtension ="pdf" : cursorExtension ="png"
+        cursorExtension ="png"
         description = spec[:description]
         cmdAdebeo = UI::Command.new(description){
           eval(spec[:command])
         }
         if spec[:toolbar]
             cmdAdebeo.tooltip = spec[:description]
-            cmdAdebeo.small_icon = "../icon/#{spec[:name]}_SM.png"
-            cmdAdebeo.large_icon = "../icon/#{spec[:name]}_BG.png"
+            cmdAdebeo.small_icon = "../icon/#{spec[:name]}_SM.#{cursorExtension}"
+            cmdAdebeo.large_icon = "../icon/#{spec[:name]}_BG.#{cursorExtension}"
             toolbar = spec[:toolbar].add_item cmdAdebeo
         end
         if spec[:submenu]
             cmdAdebeo.menu_text = spec[:description]
             spec[:submenu].add_item(cmdAdebeo)
         end
-        if spec[:subContextMenu]
-            spec[:subContextMenu].add_item(spec[:description]){
-                eval(spec[:command])
-            }
-        end
+
+        return cmdAdebeo
+
     end
 
 end
